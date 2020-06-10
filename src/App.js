1b4ad2ba7ac9e4b17154d27274a6bec7c00dda14
Warsame Osman <osman18@my.yorku.ca>
2020-06-10 16:03:38 -0400

    basic

--- src/App.js
+++ src/App.js
@@ -79,6 +79,10 @@ render() {
   return(
 
     <div>
+    <head>
+    <link rel="shortcut icon" href="post.png"/>
+  <title>180 change street survey</title>
+</head>
 <h1 className="title pointer">180 Change Street Survey</h1>
 
           {this.state.redirect === false
