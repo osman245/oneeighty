7296b11237e201045beace52d7eda8548a52edf8
Warsame Osman <osman18@my.yorku.ca>
2020-06-15 15:05:35 -0400

    chiillin

--- src/App.js
+++ src/App.js
@@ -67,12 +67,8 @@ render() {
   return(
 
     <div>
- <html>
- <head>
-<title>180 Survey</title>
-<head/>
-
-<html/>
+    
+ 
 
     
 
@@ -150,16 +146,4 @@ export default  App;
 
 
 
-  /*
-
-
-   <div className="mt4">
-        <label className=" db " htmlFor="name">Name:</label>
-        <input onChange = {this.onNameChange} className=" mv2 pa2 input-reset ba b--black hover-bg-black hover-white" type="name" name="name"  id="email-address"/>
-      </div>
-
-      <div className="mv3">
-        <label className="db " htmlFor="email-address">Email Address:</label>
-        <input onChange = {this.onEmailChange} className=" mv2 b pa2 input-reset ba b--black  hover-bg-black hover-white " type="email" name="email-address"  />
-      </div>
-      */
+ 
